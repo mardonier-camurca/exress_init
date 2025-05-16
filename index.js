221a3d56c467049673d7fd86738abd5c67a40cb2
mardonier-camurca <159277473+mardonier-camurca@users.noreply.github.com>
2025-05-16 16:40:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,48 @@ app.get('/Features', (req, res) => {
 app.get('/About', (req, res) => {
     res.send('Dev Mardonier S Camurça')
 });
+
+
+const express = require('express');
+
+const porta = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo a tela inicia do nosso site')
+});
+
+app.get('/inspectionar', (req, res) => {
+    console.log('nova requisicação recebida!')
+    console.log('method: ', req.method);
+    console.log('URL: ', req.URL);
+    console.log('method: ', req.header);
+});
+
+
+app.get('/api/produts', (res) =>  {
+    res.json([
+        {
+            "id": "1", 
+            "nome" : "notebook",
+        },
+        {
+            "id": "2", 
+            "nome" : "mouse",
+        },
+        {
+            "id": "3", 
+            "nome" : "desk",
+        },
+        {
+            "id": "4", 
+            "nome" : "monitor",
+        }
+
+    ])
+    
+});
+
+
+
